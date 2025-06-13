37331e94d97ab1676b6438186e60463220974838
Johnson <25720049+ElNosnhoj@users.noreply.github.com>
2025-06-13 10:43:46 -0700

    Update edgescare.ps1

--- edgescare.ps1
+++ edgescare.ps1
@@ -1,1 +1,1 @@
-Start-Process "msedge.exe" -ArgumentList '--kiosk https://example.com --edge-kiosk-type=fullscreen'
+Start-Process "msedge.exe" -ArgumentList '--kiosk https://raw.githack.com/ElNosnhoj/trolling/main/index.html --edge-kiosk-type=fullscreen'
